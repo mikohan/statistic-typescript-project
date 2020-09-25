16622e7709eec7f6e0fe4cbebffc927e71361f4e
mikohan <angara99@gmail.com>
2020-09-25 18:35:43 +0300

    Made refactoring static class methods

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/consoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
+
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
 summary.buildAndPrintReport(matchReader.matches);
